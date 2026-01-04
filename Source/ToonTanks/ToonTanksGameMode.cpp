f12c3dad4dcc3bdc23649d5708467917dd581544
ThiagoIanuch <thiagoianuch@hotmail.com>
2026-01-03 21:48:33 -0300

    Add player controller class

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -5,12 +5,14 @@
 #include "Kismet/GameplayStatics.h"
 #include "Tank.h"
 #include "Tower.h"
+#include "ToonTanksPlayerController.h"
 
 void AToonTanksGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+	ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
 }
 
 void AToonTanksGameMode::ActorDied(AActor* DeadActor)
@@ -19,10 +21,9 @@ void AToonTanksGameMode::ActorDied(AActor* DeadActor)
 	{
 		Tank->HandleDestruction();
 
-		if (Tank->GetTankPlayerController())
+		if (ToonTanksPlayerController)
 		{
-			Tank->DisableInput(Tank->GetTankPlayerController());
-			Tank->GetTankPlayerController()->bShowMouseCursor = false;
+			ToonTanksPlayerController->SetPlayerEnabledState(false);
 		}
 	}
 	else if (ATower* DestroyedTower = Cast<ATower>(DeadActor))
